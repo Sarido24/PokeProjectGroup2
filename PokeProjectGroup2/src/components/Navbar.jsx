dd46edff8dd08f2622630c42bcc92993d134ad58
adiatraa <adiatrahman31@gmail.com>
2024-07-03 19:53:12 +0700

    Minor Update Navbar

--- PokeProjectGroup2/src/components/Navbar.jsx
+++ PokeProjectGroup2/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { Navbar, Nav, Button } from 'react-bootstrap';
 const MainNavbar = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg" className="custom-navbar">
-      <Navbar.Brand href="#home">
+      <Navbar.Brand href="/home">
         <img
           src="logo.png"
           width="30"
@@ -18,8 +18,8 @@ const MainNavbar = () => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#about">Evolution</Nav.Link>
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/evolution">Evolution</Nav.Link>
         </Nav>
         <Button variant="success" className="create-team-btn">
           Create Team
