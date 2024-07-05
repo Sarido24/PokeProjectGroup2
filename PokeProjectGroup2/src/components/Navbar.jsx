f6b18fdf5ac6d31bdffbe0d6eb579701b1baa19d
adiatraa <adiatrahman31@gmail.com>
2024-07-05 14:38:25 +0700

    Styling Navbar Responsive on Mobile

--- PokeProjectGroup2/src/components/Navbar.jsx
+++ PokeProjectGroup2/src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-// src/Navbar.jsx
 import React from 'react';
 import { Navbar, Nav, Button, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
@@ -10,7 +9,7 @@ const MainNavbar = () => {
     <Navbar bg="dark" variant="dark" expand="lg" className={styles.customNavbar}>
       <Container fluid>
         <Navbar.Brand href="/" className={`p-2 d-flex align-items-center ${styles.navbarBrand}`}>
-          <a href="/" className="d-flex align-items-center text-decoration-none">
+          <Link to="/" className="d-flex align-items-center text-decoration-none">
             <img
               src={logo}
               width="50"
@@ -19,11 +18,11 @@ const MainNavbar = () => {
               alt="Logo"
             />
             <span className={styles.navbarBrandText}>Pokemon Dex</span>
-          </a>
+          </Link>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className={styles.navbarNav}>
-          <Nav className="">
+          <Nav className="mx-auto">
             <Nav.Link href="/" className={styles.navLink}>Home</Nav.Link>
             <Nav.Link href="/evolution" className={styles.navLink}>Evolution</Nav.Link>
             <Nav.Link href="#" className={styles.navLink}>Statistic</Nav.Link>
@@ -31,12 +30,14 @@ const MainNavbar = () => {
             <Nav.Link href="#" className={styles.navLink}>Types</Nav.Link>
             <Nav.Link href="#" className={styles.navLink}>About Us</Nav.Link>
           </Nav>
+          <div className={`d-flex align-items-center mt-3 mt-lg-0 ${styles.createTeamBtnWrapper}`}>
+            <Link to="/">
+              <Button variant="success" className={`ml-auto ${styles.createTeamBtn}`}>
+                Create Team
+              </Button>
+            </Link>
+          </div>
         </Navbar.Collapse>
-        <Link to="/">
-          <Button variant="success" className={`ml-auto ${styles.createTeamBtn}`}>
-            Create Team
-          </Button>
-        </Link>
       </Container>
     </Navbar>
   );
